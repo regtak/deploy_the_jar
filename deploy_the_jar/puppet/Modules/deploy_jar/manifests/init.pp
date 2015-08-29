04a59194b9ff8c4d06e0e0edc2c3bd9338c6934a
maxim.s <maxim.shayakhmetov@digitalaclhemy.com.au>
2015-08-29 13:58:12 +1000

    added wget

--- deploy_the_jar/puppet/Modules/deploy_jar/manifests/init.pp
+++ deploy_the_jar/puppet/Modules/deploy_jar/manifests/init.pp
@@ -6,22 +6,27 @@ class deploy_jar
         before => Package['java-1.8.0-openjdk'],
     }
   }
-  #elsif $::facts['operatingsystemmajrelease'] == '6'{
-  #  service { 'iptables':
-  #      ensure => 'stopped',
-  #      before => Package['java'],
-  #  }
-  #}
-  #elsif $::facts['operatingsystemmajrelease'] == '5'{
-  #  exec { 'stop iptables':
-  #      command => "/etc/init.d/iptables stop",  
-  #      before => Package['java'],
-  #  }
-  #}
+  elsif Facts['operatingsystemmajrelease'] == '6'{
+    service { 'iptables':
+        ensure => 'stopped',
+        before => Package['java-1.8.0-openjdk'],
+    }
+  }
+  elsif Facts['operatingsystemmajrelease'] == '5'{
+    exec { 'stop iptables':
+        command => "/etc/init.d/iptables stop",  
+        before => Package['java-1.8.0-openjdk'],
+    }
+  }
   package { 'java-1.8.0-openjdk':
         ensure => 'installed',
-        before => Exec['download_jar'] 
+        before =>  Package['wget.x86_64'],
       }
+  package { 'wget.x86_64':
+        ensure => 'installed',
+        before => Exec['download_jar'],
+      }
+      
   exec {'download_jar':
   command => "/usr/bin/wget https://s3-ap-southeast-2.amazonaws.com/covata-robertbartlett-content-objectdata/spring-boot-sample-jetty-1.0.0.RC5.jar",
   before => Exec ['run_jar']
