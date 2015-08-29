810c97150136d3e6c04e289967edfc8f7a165603
maxim.s <maxim.shayakhmetov@digitalaclhemy.com.au>
2015-08-29 19:02:07 +1000

    just iptables

--- deploy_the_jar/puppet/Modules/deploy_jar/manifests/init.pp
+++ deploy_the_jar/puppet/Modules/deploy_jar/manifests/init.pp
@@ -1,11 +1,11 @@
 class deploy_jar 
 {
-   if Facts['operatingsystemmajrelease'] == '7' {
-    service { 'firewalld':
-        ensure => 'stopped',
-        before => Package['java-1.8.0-openjdk'],
-    }
-  }
+#   if Facts['operatingsystemmajrelease'] == '7' {
+#    service { 'firewalld':
+#        ensure => 'stopped',
+#        before => Package['java-1.8.0-openjdk'],
+#    }
+#  }
   if Facts['operatingsystemmajrelease'] == '6' {
     exec { 'stop iptables':
         command => "/etc/init.d/iptables stop",
@@ -13,22 +13,22 @@ class deploy_jar
     }
   }
   
-  package { 'java-1.8.0-openjdk':
-        ensure => 'installed',
-        before =>  Package['wget.x86_64'],
-      }
-  package { 'wget.x86_64':
-        ensure => 'installed',
-        before => Exec['download_jar'],
-      }
+#  package { 'java-1.8.0-openjdk':
+#        ensure => 'installed',
+#        before =>  Package['wget.x86_64'],
+#      }
+#  package { 'wget.x86_64':
+#        ensure => 'installed',
+#        before => Exec['download_jar'],
+#      }
       
-  exec {'download_jar':
-  command => "/usr/bin/wget https://s3-ap-southeast-2.amazonaws.com/covata-robertbartlett-content-objectdata/spring-boot-sample-jetty-1.0.0.RC5.jar",
-  before => Exec ['run_jar']
-  }
-  exec {'run_jar':
-  command => "/usr/bin/java -jar spring-boot-sample-jetty-1.0.0.RC5.jar&", 
-  }
+#  exec {'download_jar':
+#  command => "/usr/bin/wget https://s3-ap-southeast-2.amazonaws.com/covata-robertbartlett-content-objectdata/spring-boot-sample-jetty-1.0.0.RC5.jar",
+#  before => Exec ['run_jar']
+# }
+#  exec {'run_jar':
+#  command => "/usr/bin/java -jar spring-boot-sample-jetty-1.0.0.RC5.jar&", 
+#  }
 }
 
 
