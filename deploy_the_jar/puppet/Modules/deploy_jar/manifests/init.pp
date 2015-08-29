aac17eff543280a808f29d868aa9fab610f12d6f
maxim.s <maxim.shayakhmetov@digitalaclhemy.com.au>
2015-08-29 11:45:58 +1000

    corrected name

--- deploy_the_jar/puppet/Modules/deploy_jar/manifests/init.pp
+++ deploy_the_jar/puppet/Modules/deploy_jar/manifests/init.pp
@@ -22,7 +22,7 @@ class deploy_jar
         ensure => 'installed',
         before => Exec['download_jar'] 
       }
-  exec {'downlaod_jar':
+  exec {'download_jar':
   command => "/usr/bin/wget https://s3-ap-southeast-2.amazonaws.com/covata-robertbartlett-content-objectdata/spring-boot-sample-jetty-1.0.0.RC5.jar",
   before => Exec ['run_jar']
   }
