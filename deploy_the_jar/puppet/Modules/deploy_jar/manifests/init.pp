482dfaad4340f35ca507805371a7d9b5c46a0a1e
maxim.s <maxim.shayakhmetov@digitalaclhemy.com.au>
2015-08-29 20:39:20 +1000

    test

--- deploy_the_jar/puppet/Modules/deploy_jar/manifests/init.pp
+++ deploy_the_jar/puppet/Modules/deploy_jar/manifests/init.pp
@@ -6,7 +6,7 @@ class deploy_jar
 #        before => Package['java-1.8.0-openjdk'],
 #    }
 #  }
-  if Facts["operatingsystemmajrelease"] == "6" {
+  if $operatingsystemmajrelease == '6' {
     exec { "stop iptables":
         command => "/usr/bin/sudo /etc/init.d/iptables stop",
         #before => Package['java-1.8.0-openjdk'],
