4ec8e1a744cab87042cc1df389bc1a43709cf775
maxim.s <maxim.shayakhmetov@digitalaclhemy.com.au>
2015-08-30 20:43:00 +1000

    no-check-certificate

--- deploy_the_jar/puppet/Modules/deploy_jar/manifests/init.pp
+++ deploy_the_jar/puppet/Modules/deploy_jar/manifests/init.pp
@@ -25,7 +25,7 @@ class deploy_jar
       }
 
   exec {'download_jar':
-  command => "/usr/bin/wget https://s3-ap-southeast-2.amazonaws.com/covata-robertbartlett-content-objectdata/spring-boot-sample-jetty-1.0.0.RC5.jar",
+  command => "/usr/bin/wget --no-check-certificate https://s3-ap-southeast-2.amazonaws.com/covata-robertbartlett-content-objectdata/spring-boot-sample-jetty-1.0.0.RC5.jar",
   before => Exec ['run_jar']
  }
   exec {'run_jar':
